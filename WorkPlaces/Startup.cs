8c8316ebdcc9bed7b29e3d7fecedd1793e40f6ef
Borislav Dostov <bdostov@gmail.com>
2021-09-16 16:56:33 +0300

    Add CORS

--- WorkPlaces/Startup.cs
+++ WorkPlaces/Startup.cs
@@ -28,6 +28,7 @@ namespace WorkPlaces
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddControllers();
 
             services.AddDbContext<ApplicationDbContext>(options =>
@@ -57,6 +58,11 @@ namespace WorkPlaces
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(builder => builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
