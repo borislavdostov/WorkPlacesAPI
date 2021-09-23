6c8563296154c9745d5ffbb61ac0143cc4d78252
Borislav Dostov <bdostov@gmail.com>
2021-09-23 12:00:23 +0300

    Update DateOfBirthAttribute.cs

--- WorkPlaces.DataModel/ValidationAttributes/DateOfBirthAttribute.cs
+++ WorkPlaces.DataModel/ValidationAttributes/DateOfBirthAttribute.cs
@@ -2,7 +2,7 @@
 using System.ComponentModel.DataAnnotations;
 using WorkPlaces.DataModel.Models;
 
-namespace WorkPlaces.Common.ValidationAttributes
+namespace WorkPlaces.DataModel.ValidationAttributes
 {
     public class DateOfBirthAttribute : ValidationAttribute
     {
