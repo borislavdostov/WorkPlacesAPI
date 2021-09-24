ca2ec208b56329294f704d08781bd402b6659c04
Borislav Dostov <bdostov@gmail.com>
2021-09-24 15:36:06 +0300

    Refactoring

--- WorkPlaces/Controllers/UserWorkPlacesController.cs
+++ WorkPlaces/Controllers/UserWorkPlacesController.cs
@@ -29,11 +29,11 @@ namespace WorkPlaces.Controllers
         /// </summary>
         /// <returns>All user work places(without deleted)</returns>
         /// <response code="200">Returns all user work places(without deleted) or empty collection</response>
-        [HttpGet(Name = nameof(GetUserWorkPlaces))]
-        public ActionResult<IEnumerable<UserWorkplaceDTO>> GetUserWorkPlaces()
+        [HttpGet(Name = nameof(GetUserWorkplaces))]
+        public ActionResult<IEnumerable<UserWorkplaceDTO>> GetUserWorkplaces()
         {
-            var userWorkPlaces = userWorkplacesService.GetUserWorkplaces();
-            return Ok(userWorkPlaces);
+            var userWorkplaces = userWorkplacesService.GetUserWorkplaces();
+            return Ok(userWorkplaces);
         }
 
         /// <summary>
@@ -42,89 +42,89 @@ namespace WorkPlaces.Controllers
         /// <returns>All options for user work place(without deleted)</returns>
         /// <response code="200">All options for user work place(without deleted) or empty collection</response>
         [HttpGet("options")]
-        public ActionResult<UserWorkplaceOptionsDTO> GetUserWorkPlaceOptions()
+        public ActionResult<UserWorkplaceOptionsDTO> GetUserWorkplaceOptions()
         {
-            var userWorkPlaceOptions = userWorkplacesService.GetUserWorkplaceOptions();
-            return Ok(userWorkPlaceOptions);
+            var userWorkplaceOptions = userWorkplacesService.GetUserWorkplaceOptions();
+            return Ok(userWorkplaceOptions);
         }
 
         /// <summary>
         /// Gets specific user workplace
         /// </summary>
-        /// <param name="userWorkPlaceId">Id of the user workplace</param>
+        /// <param name="userWorkplaceId">Id of the user workplace</param>
         /// <returns>The user workplace with the given Id</returns>
         /// <response code="200">Returns a user workplace with the given Id</response>
         /// <response code="404">If a user workplace with the given id does not exist</response>
-        [HttpGet("{userWorkPlaceId}")]
-        public async Task<ActionResult<UserWorkplaceForManipulationDTO>> GetUserWorkPlace(int userWorkPlaceId)
+        [HttpGet("{userWorkplaceId}")]
+        public async Task<ActionResult<UserWorkplaceForManipulationDTO>> GetUserWorkplace(int userWorkplaceId)
         {
-            if (!await userWorkplacesService.UserWorkplaceExistsAsync(userWorkPlaceId))
+            if (!await userWorkplacesService.UserWorkplaceExistsAsync(userWorkplaceId))
             {
                 return NotFound();
             }
 
-            var userWorkPlace = await userWorkplacesService.GetUserWorkplaceAsync(userWorkPlaceId);
-            return Ok(userWorkPlace);
+            var userWorkplace = await userWorkplacesService.GetUserWorkplaceAsync(userWorkplaceId);
+            return Ok(userWorkplace);
         }
 
         /// <summary>
         /// Creates new user workplace
         /// </summary>
-        /// <param name="userWorkPlace"></param>
+        /// <param name="userWorkplace"></param>
         /// <returns>A newly created user workplace</returns>
         /// <response code="201">Returns the newly created user workplace</response>
         [HttpPost]
-        public async Task<ActionResult<UserWorkplaceDTO>> CreateUserWorkPlace(UserWorkplaceForManipulationDTO userWorkPlace)
+        public async Task<ActionResult<UserWorkplaceDTO>> CreateUserWorkplace(UserWorkplaceForManipulationDTO userWorkplace)
         {
-            if (!await usersService.UserExistsAsync(userWorkPlace.UserId) ||
-                !await workplacesService.WorkplaceExistsAsync(userWorkPlace.WorkplaceId))
+            if (!await usersService.UserExistsAsync(userWorkplace.UserId) ||
+                !await workplacesService.WorkplaceExistsAsync(userWorkplace.WorkplaceId))
             {
                 return NotFound();
             }
 
-            var userWorkPlaceToReturn = await userWorkplacesService.CreateUserWorkplaceAsync(userWorkPlace);
-            return CreatedAtRoute(nameof(GetUserWorkPlaces),
-                new { userWorkPlaceId = userWorkPlaceToReturn.Id }, userWorkPlaceToReturn);
+            var userWorkplaceToReturn = await userWorkplacesService.CreateUserWorkplaceAsync(userWorkplace);
+            return CreatedAtRoute(nameof(GetUserWorkplaces),
+                new { userWorkplaceId = userWorkplaceToReturn.Id }, userWorkplaceToReturn);
         }
 
         /// <summary>
         /// Updates user workplace
         /// </summary>
-        /// <param name="userWorkPlaceId">Id of the user workplace</param>
-        /// <param name="userWorkPlace"></param>
+        /// <param name="userWorkplaceId">Id of the user workplace</param>
+        /// <param name="userWorkplace"></param>
         /// <returns>No content</returns>
         /// <response code="204">If the user workplace is updated successfully</response>
         /// <response code="404">If a user workplace, user or workplace with the given id does not exist</response>
-        [HttpPut("{userWorkPlaceId}")]
-        public async Task<IActionResult> UpdateUserWorkPlace(int userWorkPlaceId, UserWorkplaceForManipulationDTO userWorkPlace)
+        [HttpPut("{userWorkplaceId}")]
+        public async Task<IActionResult> UpdateUserWorkPlace(int userWorkplaceId, UserWorkplaceForManipulationDTO userWorkplace)
         {
-            if (!await userWorkplacesService.UserWorkplaceExistsAsync(userWorkPlaceId) ||
-                !await usersService.UserExistsAsync(userWorkPlace.UserId) ||
-                !await workplacesService.WorkplaceExistsAsync(userWorkPlace.WorkplaceId))
+            if (!await userWorkplacesService.UserWorkplaceExistsAsync(userWorkplaceId) ||
+                !await usersService.UserExistsAsync(userWorkplace.UserId) ||
+                !await workplacesService.WorkplaceExistsAsync(userWorkplace.WorkplaceId))
             {
                 return NotFound();
             }
 
-            await userWorkplacesService.UpdateUserWorkplaceAsync(userWorkPlaceId, userWorkPlace);
+            await userWorkplacesService.UpdateUserWorkplaceAsync(userWorkplaceId, userWorkplace);
             return NoContent();
         }
 
         /// <summary>
         /// Deletes user workplace
         /// </summary>
-        /// <param name="userWorkPlaceId">Id of the user workplace</param>
+        /// <param name="userWorkplaceId">Id of the user workplace</param>
         /// <returns>No content</returns>
         /// <response code="204">If the user workplace is deleted successfully</response>
         /// <response code="404">If a user workplace with the given id does not exist</response>
-        [HttpDelete("{userWorkPlaceId}")]
-        public async Task<IActionResult> DeleteUserWorkPlace(int userWorkPlaceId)
+        [HttpDelete("{userWorkplaceId}")]
+        public async Task<IActionResult> DeleteUserWorkPlace(int userWorkplaceId)
         {
-            if (!await userWorkplacesService.UserWorkplaceExistsAsync(userWorkPlaceId))
+            if (!await userWorkplacesService.UserWorkplaceExistsAsync(userWorkplaceId))
             {
                 return NotFound();
             }
 
-            await userWorkplacesService.DeleteUserWorkplaceAsync(userWorkPlaceId);
+            await userWorkplacesService.DeleteUserWorkplaceAsync(userWorkplaceId);
             return NoContent();
         }
     }
