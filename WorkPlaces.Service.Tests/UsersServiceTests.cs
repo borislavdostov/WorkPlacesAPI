cfe4f3c6f9c97dcaa612d57cdad82fe13c96d764
Borislav Dostov <bdostov@gmail.com>
2021-09-30 16:17:18 +0300

    Add test for users service, getAll method

---
+++ WorkPlaces.Service.Tests/UsersServiceTests.cs
@@ -0,0 +1,43 @@
+﻿using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using WorkPlaces.Data.Entities;
+using WorkPlaces.Data.Interfaces;
+using WorkPlaces.DataModel.Models;
+using WorkPlaces.Service.Interfaces;
+using WorkPlaces.Service.Services;
+
+namespace WorkPlaces.Service.Tests
+{
+    [TestFixture]
+    public class UsersServiceTests
+    {
+        private IUsersService usersService;
+        private List<User> usersFromRepository;
+        private Mock<IUsersRepository> mockUsersRepository;
+        private IUsersRepository usersRepository;
+        private IQueryable<User> QueryableUsers => usersFromRepository.AsQueryable();
+
+        [SetUp]
+        public void Initialize()
+        {
+            usersFromRepository = new List<User>();
+            mockUsersRepository = new Mock<IUsersRepository>();
+            mockUsersRepository.Setup(r => r.GetAll()).Returns(QueryableUsers);
+            usersRepository = mockUsersRepository.Object;
+            usersService = new UsersService(usersRepository);
+        }
+
+        [Test]
+        public void GetUsers_WithOneUser_ShouldReturnUsersCorrectly()
+        {
+            usersFromRepository.Add(new User());
+            var expectedResult = new List<UserDTO> { new UserDTO() };
+
+            var users = usersService.GetUsers();
+
+            Assert.AreEqual(expectedResult.Count, users.Count());
+        }
+    }
+}
