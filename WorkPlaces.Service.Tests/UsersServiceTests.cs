7a59a2f6c19ef39df480935781b9dbdb8d9dd80d
Borislav Dostov <bdostov@gmail.com>
2021-09-30 16:44:45 +0300

    Update UsersServiceTests.cs

--- WorkPlaces.Service.Tests/UsersServiceTests.cs
+++ WorkPlaces.Service.Tests/UsersServiceTests.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using WorkPlaces.Data.Entities;
 using WorkPlaces.Data.Interfaces;
-using WorkPlaces.DataModel.Models;
 using WorkPlaces.Service.Interfaces;
 using WorkPlaces.Service.Services;
 
