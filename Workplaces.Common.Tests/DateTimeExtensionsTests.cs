4981e605588d8e6dc9cd8ea82f849991a573a1cc
Borislav Dostov <bdostov@gmail.com>
2021-10-01 14:31:59 +0300

    Add test project for common project

---
+++ Workplaces.Common.Tests/DateTimeExtensionsTests.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using System;
+using Workplaces.Extensions;
+
+namespace Workplaces.Common.Tests
+{
+    [TestFixture]
+    public class DateTimeExtensionsTests
+    {
+        [Test]
+        public void GetAgeMethod_WithAgeTwentyFive_ShouldReturnAgeCorrectly()
+        {
+            var date = new DateTime(1996, 4, 27);
+
+            var actualResult = date.GetAge();
+
+            Assert.AreEqual(25, actualResult);
+        }
+
+        [Test]
+        public void GetAgeMethod_WithLeapYear_ShouldReturnAgeCorrectly()
+        {
+            var date = new DateTime(2004, 2, 29);
+
+            var actualResult = date.GetAge();
+
+            Assert.AreEqual(17, actualResult);
+        }
+
+        [Test]
+        public void GetAgeMethod_WithAgeZero_ShouldReturnZeroAge()
+        {
+            var date = DateTime.Now;
+
+            var actualResult = date.GetAge();
+
+            Assert.AreEqual(0, actualResult);
+        }
+
+        [Test]
+        public void GetAgeMethod_WithNegativeAge_ShouldReturnNegativeAge()
+        {
+            var date = new DateTime(2025, 3, 3);
+
+            var actualResult = date.GetAge();
+
+            Assert.AreEqual(-4, actualResult);
+        }
+    }
+}
