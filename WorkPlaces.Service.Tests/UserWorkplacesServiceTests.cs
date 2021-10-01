cbac81d52e34f5e6e35bb81c1a5551b6ae9e6777
Borislav Dostov <bdostov@gmail.com>
2021-10-01 10:27:53 +0300

    Create test file for UserWorkPlaceService

---
+++ WorkPlaces.Service.Tests/UserWorkplacesServiceTests.cs
@@ -0,0 +1,37 @@
+﻿using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using Workplaces.Data.Entities;
+using Workplaces.Data.Interfaces;
+using Workplaces.Data.Repositories;
+using Workplaces.Service.Interfaces;
+using Workplaces.Service.Services;
+
+namespace Workplaces.Service.Tests
+{
+    [TestFixture]
+    public class UserWorkplacesServiceTests
+    {
+        IUserWorkplacesService userWorkplacesService;
+        Mock<IUserWorkplacesRepository> mockUserWorkplacesRepository;
+        List<UserWorkplace> userWorkplacesFromRepository;
+        Mock<IUsersRepository> mockUsersRepository;
+        List<User> usersFromRepository;
+        Mock<IWorkplacesRepository> mockWorkplacesRepository;
+        List<Workplace> workplacesFromRepository;
+
+        [SetUp]
+        public void Initialize()
+        {
+            mockUserWorkplacesRepository = new Mock<IUserWorkplacesRepository>();
+            userWorkplacesFromRepository = new List<UserWorkplace>();
+            mockUsersRepository = new Mock<IUsersRepository>();
+            usersFromRepository = new List<User>();
+            mockWorkplacesRepository = new Mock<IWorkplacesRepository>();
+            workplacesFromRepository = new List<Workplace>();
+
+            userWorkplacesService = new UserWorkplacesService(
+                mockUserWorkplacesRepository.Object, mockUsersRepository.Object, mockWorkplacesRepository.Object);
+        }
+    }
+}
