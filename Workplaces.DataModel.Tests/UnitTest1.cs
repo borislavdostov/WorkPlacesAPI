38b8eb469ade3583d8e86b3ad991977a378b22ef
Borislav Dostov <bdostov@gmail.com>
2021-10-01 14:34:20 +0300

    Add test project for data model project

---
+++ Workplaces.DataModel.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Workplaces.DataModel.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
