05fe984283c2669cdbe47fcbc60f42368714db18
Borislav Dostov <bdostov@gmail.com>
2021-10-01 15:49:51 +0300

    Add tests for PersonNameAttribute

--- Workplaces.DataModel.Tests/PersonNameAttributeTests.cs
+++ Workplaces.DataModel.Tests/PersonNameAttributeTests.cs
@@ -1,10 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using NUnit.Framework;
+using System.ComponentModel.DataAnnotations;
+using Workplaces.DataModel.ValidationAttributes;
 
 namespace Workplaces.DataModel.Tests
 {
-    class PersonNameAttributeTests
+    [TestFixture]
+    public class PersonNameAttributeTests
     {
+        PersonNameAttribute personNameAttribute;
+
+        [SetUp]
+        public void Initialize()
+        {
+            personNameAttribute = new PersonNameAttribute();
+        }
+
+        [TestCase("Peter")]
+        [TestCase("O Henry")]
+        [TestCase("O'Riley")]
+        [TestCase("HAALAND")]
+        [TestCase("McDonald")]
+        public void IsValidMethod_WithCorrectName_ShouldReturnTrue(string name)
+        {
+            var validationResult = personNameAttribute.GetValidationResult(name, new ValidationContext(name));
+            var actualResult = validationResult == ValidationResult.Success;
+
+            Assert.IsTrue(actualResult);
+        }
+
+        [TestCase("peter")]
+        [TestCase("o Henry")]
+        [TestCase("o'Riley")]
+        [TestCase("hAALAND")]
+        [TestCase("mcDonald")]
+        [TestCase("1Peter")]
+        [TestCase("@Peter")]
+        public void IsValidMethod_WithIncorrectName_ShouldReturnFalse(string name)
+        {
+            var validationResult = personNameAttribute.GetValidationResult(name, new ValidationContext(name));
+            var actualResult = validationResult == ValidationResult.Success;
+
+            Assert.IsFalse(actualResult);
+        }
+
+        [Test]
+        public void IsValidMethod_WithIncorrectName_ShouldReturnErrorMessage()
+        {
+            var name = "john";
+
+            var validationResult = personNameAttribute.GetValidationResult(name, new ValidationContext(name));
+            var actualResult = validationResult.ErrorMessage;
+
+            Assert.AreEqual(
+                "Name should start with a capital letter and cannot contain numbers or any special symbols.", actualResult);
+        }
     }
 }
