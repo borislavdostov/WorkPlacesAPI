3112e02e8e84af78d54db369d518df0d13273c56
Borislav Dostov <bdostov@gmail.com>
2021-10-04 14:44:06 +0300

    Update UsersControllerTests.cs

--- Workplaces.Tests/UsersControllerTests.cs
+++ Workplaces.Tests/UsersControllerTests.cs
@@ -1,6 +1,8 @@
-﻿using Moq;
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
 using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 using Workplaces.Controllers;
 using Workplaces.DataModel.Models;
 using Workplaces.Service.Interfaces;
@@ -18,7 +20,11 @@ namespace Workplaces.Tests
         [SetUp]
         public void Initialize()
         {
+            usersFromService = new List<UserDTO>();
             mockUsersService = new Mock<IUsersService>();
+            mockUsersService.Setup(s => s.GetUsers())
+                .Returns(usersFromService);
+
             usersController = new UsersController(mockUsersService.Object);
         }
 
@@ -26,6 +32,17 @@ namespace Workplaces.Tests
         public void Constructor_WithNullUsersService_ShouldThrowArgumentNullException()
         {
             Assert.Throws<ArgumentNullException>(() => usersController = new UsersController(null));
+        }
+
+        [Test]
+        public void GetUsersMethod_WithTwoUsers_ShouldReturnOK()
+        {
+            usersFromService.Add(new UserDTO { Id = 1 });
+            usersFromService.Add(new UserDTO { Id = 2 });
+
+            var actualResult = usersController.GetUsers();
+
+            Assert.IsInstanceOf<OkObjectResult>(actualResult.Result);
         }
 
 
