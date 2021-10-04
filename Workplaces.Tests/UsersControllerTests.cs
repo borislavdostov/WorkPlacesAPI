007ecc604196a72805e577255a350a6a2ba36690
Borislav Dostov <bdostov@gmail.com>
2021-10-04 14:16:52 +0300

    Update UsersControllerTests.cs

--- Workplaces.Tests/UsersControllerTests.cs
+++ Workplaces.Tests/UsersControllerTests.cs
@@ -1,20 +1,24 @@
 ﻿using NUnit.Framework;
+using System;
+using Workplaces.Controllers;
 
 namespace Workplaces.Tests
 {
     [TestFixture]
     public class UsersControllerTests
     {
+        private UsersController usersController;
+
         [SetUp]
         public void Initialize()
         {
-
+            //usersController = new UsersController(null);
         }
 
         [Test]
         public void Constructor_WithNullUsersService_ShouldThrowArgumentNullException()
         {
-
+            Assert.Throws<ArgumentNullException>(() => usersController = new UsersController(null));
         }
     }
 }
