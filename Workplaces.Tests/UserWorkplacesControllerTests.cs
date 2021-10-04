6bca90bc74ab76da25cb1302eaf374d8ce5cd6e1
Borislav Dostov <bdostov@gmail.com>
2021-10-04 15:26:46 +0300

    Update UserWorkplacesControllerTests.cs

--- Workplaces.Tests/UserWorkplacesControllerTests.cs
+++ Workplaces.Tests/UserWorkplacesControllerTests.cs
@@ -2,5 +2,20 @@
 {
     public class UserWorkplacesControllerTests
     {
+        //Declarations
+
+        //SetUp method
+
+        //Constructor
+
+        //Get UserWorkplaces
+
+        //Get UserWorkplace
+
+        //Create UserWorkplace
+
+        //Update UserWorkplace
+
+        //Delete UserWorkplace
     }
 }
