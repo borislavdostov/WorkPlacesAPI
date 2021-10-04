c452ecadc0fb60e8c79b316ee5b8e6d8ea16c88a
Borislav Dostov <bdostov@gmail.com>
2021-10-04 15:20:56 +0300

    Update UsersControllerTests.cs

--- Workplaces.Tests/UsersControllerTests.cs
+++ Workplaces.Tests/UsersControllerTests.cs
@@ -3,8 +3,11 @@ using Moq;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Workplaces.Controllers;
+using Workplaces.Data.Entities;
 using Workplaces.DataModel.Models;
+using Workplaces.Extensions;
 using Workplaces.Service.Interfaces;
 
 namespace Workplaces.Tests
@@ -15,15 +18,32 @@ namespace Workplaces.Tests
         private UsersController usersController;
 
         private Mock<IUsersService> mockUsersService;
-        private List<UserDTO> usersFromService;
+        private List<User> usersFromService;
 
         [SetUp]
         public void Initialize()
         {
-            usersFromService = new List<UserDTO>();
+            usersFromService = new List<User>();
             mockUsersService = new Mock<IUsersService>();
             mockUsersService.Setup(s => s.GetUsers())
-                .Returns(usersFromService);
+                .Returns(usersFromService.Select(u => new UserDTO
+                {
+                    Id = u.Id,
+                    Name = $"{u.FirstName} {u.LastName}",
+                    Email = u.Email,
+                    Age = u.DateOfBirth.GetAge()
+                }));
+            mockUsersService.Setup(s => s.GetUserAsync(It.IsAny<int>()))
+                .ReturnsAsync((int id) => usersFromService.Where(u => u.Id == id)
+                .Select(u => new UserForManipulationDTO
+                {
+                    FirstName = u.FirstName,
+                    LastName = u.LastName,
+                    Email = u.Email,
+                    DateOfBirth = u.DateOfBirth,
+                }).Single());
+            mockUsersService.Setup(s => s.UserExistsAsync(It.IsAny<int>()))
+                .ReturnsAsync((int id) => usersFromService.Any(u => u.Id == id));
 
             usersController = new UsersController(mockUsersService.Object);
         }
@@ -37,14 +57,22 @@ namespace Workplaces.Tests
         [Test]
         public void GetUsersMethod_WithTwoUsers_ShouldReturnOK()
         {
-            usersFromService.Add(new UserDTO { Id = 1 });
-            usersFromService.Add(new UserDTO { Id = 2 });
+            usersFromService.Add(new User { Id = 1 });
+            usersFromService.Add(new User { Id = 2 });
 
-            var actualResult = usersController.GetUsers();
+            var actualResult = usersController.GetUsers().Result;
 
-            Assert.IsInstanceOf<OkObjectResult>(actualResult.Result);
+            Assert.IsInstanceOf<OkObjectResult>(actualResult);
         }
 
+        [Test]
+        public void GetUserMethod_WithExistingUser_ShouldReturnOK()
+        {
+            usersFromService.Add(new User { Id = 1 });
 
+            var actualResult = usersController.GetUser(1).Result.Result;
+
+            Assert.IsInstanceOf<OkObjectResult>(actualResult);
+        }
     }
 }
