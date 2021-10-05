db765411aaeeab0b315bdd747de4d0f7ebd4a40d
Borislav Dostov <bdostov@gmail.com>
2021-10-05 11:15:30 +0300

    Create ValidationMessage.cs

---
+++ WorkPlaces.DataModel/ValidationMessage.cs
@@ -0,0 +1,8 @@
+﻿namespace Workplaces.DataModel
+{
+    public class ValidationMessage
+    {
+        public const string DateOfBirthShouldBeLowerThanTodaysDate = "Date of birth should be lower than today's date.";
+        public const string UserIsNotOldEnoughToWork = "User is not old enough to work.";
+    }
+}
