e318e47edc1ecbd718756aeff8dda532c89d17e8
Borislav Dostov <bdostov@gmail.com>
2021-10-05 11:25:07 +0300

    Update GlobalConstants.cs

--- WorkPlaces.Common/Constants/GlobalConstants.cs
+++ WorkPlaces.Common/Constants/GlobalConstants.cs
@@ -5,6 +5,7 @@
         public const string XMLDocument = "Workplaces.xml";
         public const string SwaggerTitle = "Workplaces API";
         public const string SwaggerVersion = "v1";
+        public const string SwaggerTitleWithVersion = SwaggerTitle + SwaggerVersion;
         public const string SwaggerURL = "/swagger/v1/swagger.json";
     }
 }
