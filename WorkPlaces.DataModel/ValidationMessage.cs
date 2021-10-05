f733e118c83e4f051b4d76f10f1387dfea86ec28
Borislav Dostov <bdostov@gmail.com>
2021-10-05 11:16:33 +0300

    Update ValidationMessage.cs

--- WorkPlaces.DataModel/ValidationMessage.cs
+++ WorkPlaces.DataModel/ValidationMessage.cs
@@ -2,7 +2,11 @@
 {
     public class ValidationMessage
     {
-        public const string DateOfBirthShouldBeLowerThanTodaysDate = "Date of birth should be lower than today's date.";
-        public const string UserIsNotOldEnoughToWork = "User is not old enough to work.";
+        public const string IncorrectDateOfBirth =
+            "Date of birth should be lower than today's date.";
+        public const string UserNotOldEnough =
+            "User is not old enough to work.";
+        public const string IncorrectName =
+            "Name should start with a capital letter and cannot contain numbers or any special symbols.";
     }
 }
