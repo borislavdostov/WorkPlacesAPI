db4db1056fc36ad5eea20a3bdd90418be889a426
Borislav Dostov <bdostov@gmail.com>
2021-10-05 10:37:49 +0300

    Update PersonNameAttributeTests.cs

--- Workplaces.DataModel.Tests/PersonNameAttributeTests.cs
+++ Workplaces.DataModel.Tests/PersonNameAttributeTests.cs
@@ -15,18 +15,6 @@ namespace Workplaces.DataModel.Tests
             personNameAttribute = new PersonNameAttribute();
         }
 
-        [TestCase("Peter")]
-        [TestCase("O'Riley")]
-        [TestCase("McDonald")]
-        [TestCase("Jan van")]
-        public void IsValid_WithCorrectName_ShouldReturnTrue(string name)
-        {
-            var validationResult = personNameAttribute.GetValidationResult(name, new ValidationContext(name));
-            var actualResult = validationResult == ValidationResult.Success;
-
-            Assert.IsTrue(actualResult);
-        }
-
         [TestCase("peter")]
         [TestCase("o Henry")]
         [TestCase("o'Riley")]
@@ -54,6 +42,18 @@ namespace Workplaces.DataModel.Tests
 
             Assert.AreEqual(
                 "Name should start with a capital letter and cannot contain numbers or any special symbols.", actualResult);
+        }
+
+        [TestCase("Peter")]
+        [TestCase("O'Riley")]
+        [TestCase("McDonald")]
+        [TestCase("Jan van")]
+        public void IsValid_WithCorrectName_ShouldReturnTrue(string name)
+        {
+            var validationResult = personNameAttribute.GetValidationResult(name, new ValidationContext(name));
+            var actualResult = validationResult == ValidationResult.Success;
+
+            Assert.IsTrue(actualResult);
         }
     }
 }
