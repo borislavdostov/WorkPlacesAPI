ad3fc4d588e3f162dab432b895bb135e2fe49e3f
Borislav Dostov <bdostov@gmail.com>
2021-10-05 09:19:33 +0300

    Update UserWorkplacesControllerTests.cs

--- Workplaces.Tests/UserWorkplacesControllerTests.cs
+++ Workplaces.Tests/UserWorkplacesControllerTests.cs
@@ -1,8 +1,23 @@
-﻿namespace Workplaces.Tests
+﻿using Moq;
+using System.Collections.Generic;
+using Workplaces.Controllers;
+using Workplaces.Data.Entities;
+using Workplaces.Service.Interfaces;
+
+namespace Workplaces.Tests
 {
     public class UserWorkplacesControllerTests
     {
-        //Declarations
+        private UserWorkplacesController userWorkplacesController;
+
+        private Mock<IUserWorkplacesService> mockUserWorkplacesService;
+        private List<UserWorkplace> userWorkplacesFromService;
+
+        private Mock<IUsersService> mockUsersService;
+        private List<User> usersFromService;
+
+        private Mock<IWorkplacesService> mockWorkplacesService;
+        private List<Workplace> workplacesFromService;
 
         //SetUp method
 
