f2955e37b25a1b9b613c0c60665e5f77a60e93af
Borislav Dostov <bdostov@gmail.com>
2021-10-07 11:02:19 +0300

    Seed data when app is started

--- Workplaces.Data/ApplicationDbContext.cs
+++ Workplaces.Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System;
 using Workplaces.Data.Entities;
 
 namespace Workplaces.Data
@@ -15,5 +16,15 @@ namespace Workplaces.Data
         public DbSet<Workplace> Workplaces { get; set; }
 
         public DbSet<UserWorkplace> UserWorkplaces { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Workplace>().HasData(
+                new Workplace { Id = 1, Name = "Web Developer", CreatedAt = DateTime.Now },
+                new Workplace { Id = 2, Name = "QA Specialist", CreatedAt = DateTime.Now },
+                new Workplace { Id = 3, Name = "Mobile Developer", CreatedAt = DateTime.Now },
+                new Workplace { Id = 4, Name = "Full Stack Developer", CreatedAt = DateTime.Now }
+                );
+        }
     }
 }
---
+++ Workplaces.Data/Migrations/20211007075929_SeedDataMigration.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Workplaces.Data.Migrations
+{
+    public partial class SeedDataMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Workplaces",
+                columns: new[] { "Id", "CreatedAt", "DeletedAt", "Name", "UpdatedAt" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2021, 10, 7, 10, 59, 29, 450, DateTimeKind.Local).AddTicks(3156), null, "Web Developer", null },
+                    { 2, new DateTime(2021, 10, 7, 10, 59, 29, 453, DateTimeKind.Local).AddTicks(9190), null, "QA Specialist", null },
+                    { 3, new DateTime(2021, 10, 7, 10, 59, 29, 453, DateTimeKind.Local).AddTicks(9242), null, "Mobile Developer", null },
+                    { 4, new DateTime(2021, 10, 7, 10, 59, 29, 453, DateTimeKind.Local).AddTicks(9251), null, "Full Stack Developer", null }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Workplaces",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Workplaces",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Workplaces",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Workplaces",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
